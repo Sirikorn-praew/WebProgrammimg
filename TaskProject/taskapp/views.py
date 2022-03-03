bd0d5745cd03b2fd458bba4c374ec734793b2874
Sirikorn-praew <56629248+Sirikorn-praew@users.noreply.github.com>
2022-03-04 02:42:59 +0700

    update

--- TaskProject/taskapp/views.py
+++ TaskProject/taskapp/views.py
@@ -1,11 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
+from .models import Task
 
-def calculate():
-    x = 1
-    y = 2
-    return x
 
-def say_hello(request):
-    x = calculate()
-    return render(request, 'hello.html')
+def index(request):
+    tasks = Task.objects.all()
+    if request.method == "POST":
+        if "addTask" in request.POST:
+            title = request.POST["title"]
+            task = Task(title=title)
+            task.save()
+            return redirect("/")
+        if "deleteTask" in request.POST:
+            taskID = request.POST["deleteTask"]
+            task = Task.objects.get(id=int(taskID))
+            task.delete()
+    return render(request, 'main.html', {"tasks":tasks})
